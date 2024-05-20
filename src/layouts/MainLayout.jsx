6c7b4012760427f0bbd33e39594297125f9f8bd8
Yanis Ahmed <yanisahmed12@gmail.com>
2024-05-21 01:41:10 +0600

    Folder structure added

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Outlet from 'react-router-dom'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+export default function MainLayout() {
+    return (
+        <>
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}

c399307faf512c123ae1d4f01480780dd7dd554b
Yanis Ahmed <yanisahmed12@gmail.com>
2024-05-21 12:24:51 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCtMQ1wVoQzIFHuikbIkTdQlkzydjRXUJw",
+    authDomain: "stride-71d34.firebaseapp.com",
+    projectId: "stride-71d34",
+    storageBucket: "stride-71d34.appspot.com",
+    messagingSenderId: "808242000731",
+    appId: "1:808242000731:web:39ae012c8953c838bb207e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
